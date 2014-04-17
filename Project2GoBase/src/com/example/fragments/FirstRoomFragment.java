a04a70e8efcd28775a31d2898a3177d62f37eebe
weto1012 <weto1012@hs-karlsruhe.de>
2014-04-17 21:35:16 +0200

    GLSurfaceView eingebunden
    Das Fragment FirstRoomFragment hat nun als View die MyGlSurfaceView.

--- Project2GoBase/src/com/example/fragments/FirstRoomFragment.java
+++ Project2GoBase/src/com/example/fragments/FirstRoomFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.gltechdemo.MyGLSurfaceView;
 import com.example.project2Go.FirstRoomActivity;
 import com.example.project2Go.MainActivity;
 import com.example.project2Go.R;
@@ -17,6 +18,7 @@ import com.example.project2Go.R;
 	
 	public static final String ARG_SECTION_TEXT = "section_text";
 	private MainActivity mainActivity;
+	private MyGLSurfaceView mGLView;
 	
 	@SuppressLint("ValidFragment") public FirstRoomFragment(MainActivity ma) {
 		mainActivity = ma;
@@ -26,19 +28,20 @@ import com.example.project2Go.R;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 		
-		final Intent intent = new Intent(mainActivity, FirstRoomActivity.class);
-        final Button button = (Button) mainActivity.findViewById(R.id.button1);
-        button.setOnClickListener( new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(intent);
-            }
-        });
+//		final Intent intent = new Intent(mainActivity, FirstRoomActivity.class);
+//        final Button button = (Button) mainActivity.findViewById(R.id.button1);
+//        button.setOnClickListener( new View.OnClickListener() {
+//            public void onClick(View v) {
+//                startActivity(intent);
+//            }
+//        });
 		
         // TODO: Add Tobis MyGLSurfaceView
+		mGLView = new MyGLSurfaceView(inflater.getContext());
 		View rootView = inflater.inflate(com.example.project2Go.R.layout.fragment_first_room, container, false);
 		//TextView firstFragmentTextView = (TextView) rootView.findViewById(R.id.section_label);
 		//firstFragmentTextView.append(getArguments().getString(ARG_SECTION_TEXT));
 		
-		return rootView;
+		return mGLView;
 	}
 }
